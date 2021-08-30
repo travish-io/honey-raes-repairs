6190e13a631e34ce6004673b944e0061a44aecc4
Travis Hall <t.hall89d@gmail.com>
2021-08-30 14:16:08 -0400

    first commit

---
+++ src/components/Repairs.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { CustomerList } from "./customers/CustomerList";
+
+export const Repairs = () => {
+  return (
+    <>
+      <h1> Honey Rae's Repair Shop</h1>;
+      <CustomerList />
+    </>
+  );
+};
